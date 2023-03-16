24976347f06d87ca1a3d900f596e6d91b6f87336
Matt Cholick <cholick@github.com>
2023-03-16 06:50:59 -0700

    Additional global git setting

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 git config --global init.defaultBranch main
+git config --global push.default simple
 git config --global push.autoSetupRemote true
 
 export FEATURE_DAEMONIZE_SCRIPT_SERVER=true
