8fb491a4c8b288451c30fefb9d2424bae8b25e5f
Matt Cholick <cholick@gmail.com>
2023-05-12 06:08:12 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,5 +4,7 @@ git config --global init.defaultBranch main
 git config --global push.default simple
 git config --global push.autoSetupRemote true
 
+# todo: this is just a reminder, exports here don't actually work
 export FEATURE_DAEMONIZE_SCRIPT_SERVER=true
 export SERVICEOWNERS_SKIP=1
+export RUBOCOP_SKIP=1
