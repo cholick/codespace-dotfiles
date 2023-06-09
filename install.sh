aa9faee7dcc2547c4d1cc18e83ca4b4c1b7e0edc
Matt Cholick <cholick@gmail.com>
2023-06-09 09:10:04 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,8 +4,7 @@ git config --global init.defaultBranch main
 git config --global push.default simple
 git config --global push.autoSetupRemote true
 
-# This is just a reminder that I have these set as secrets, setting them here doesn't do anything
-FEATURE_DAEMONIZE_SCRIPT_SERVER=true
-DX_TELEMETRY_OPT_OUT=true
-SERVICEOWNERS_SKIP=1
-RUBOCOP_SKIP=1
+export FEATURE_DAEMONIZE_SCRIPT_SERVER=true
+export DX_TELEMETRY_OPT_OUT=true
+export SERVICEOWNERS_SKIP=1
+export RUBOCOP_SKIP=1
