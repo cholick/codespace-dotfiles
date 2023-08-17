7d54884e43546b036a7b43f6d5b0b08779e3607a
Matt Cholick <cholick@gmail.com>
2023-08-17 13:11:14 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,4 +4,5 @@ git config --global init.defaultBranch main
 git config --global push.default simple
 git config --global push.autoSetupRemote true
 
+# Leave what's in place there but append customizations
 echo "source '/workspaces/.codespaces/.persistedshare/dotfiles/.bashrc'" >> ~/.bashrc
