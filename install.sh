768de820c8f4943aca34444352e1b6982af80507
Matt Cholick <cholick@github.com>
2023-08-17 06:54:03 -0700

    WIP on more customizations

--- install.sh
+++ install.sh
@@ -9,7 +9,3 @@ export DX_TELEMETRY_OPT_OUT=true
 export SERVICEOWNERS_SKIP=1
 export RUBOCOP_SKIP=1
 export HISTFILE=/workspaces/.codespaces/.persistedshare
-
-# Gateway setting sync doesn't work yet
-mkdir -p /home/codespace/.config/JetBrains/RemoteDev-GO/_workspaces_hubber-codespace/options
-cp jetbrains-config/editor.xml /home/codespace/.config/JetBrains/RemoteDev-GO/_workspaces_hubber-codespace/options
