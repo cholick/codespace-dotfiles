a7d38e0eaf204520e6da36c090bbb0ce4da5ef1d
Matt Cholick <cholick@gmail.com>
2025-08-07 20:04:59 +0000

    add yolo settings

--- install.sh
+++ install.sh
@@ -6,4 +6,15 @@ git config --global push.autoSetupRemote true
 git config --global commit.gpgsign false
 
 # Leave what's in place there but append customizations
+
 echo "source '/workspaces/.codespaces/.persistedshare/dotfiles/.bashrc'" >> ~/.bashrc
+
+remote_settings_file="/home/vscode/.vscode-remote/data/Machine/settings.json"
+yolo_json=$(cat <<'EOF'
+	"chat.tools.terminal.autoApprove": {
+		"/.*/": true
+	},
+EOF
+)
+
+sed -i "0,/{/{s/{/{\n$yolo_json/}" "$remote_settings_file"
